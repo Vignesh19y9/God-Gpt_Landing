7f4fb51bc39079562d34b417f67d09bcea3cfa4b
Vignesh <viwanathan.vignesh@heb.com>
2023-10-20 22:33:53 +0530

    Animation Fade added

--- js/script.js
+++ js/script.js
@@ -201,6 +201,23 @@ var wordChange = function () {
   }, 1000);
 };
 
+// $(document).ready(function () {
+//   wordChange();
+// });
+
+/* FadeIn Scroll */
 $(document).ready(function () {
-  wordChange();
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+    /* Check the location of each desired element */
+    $(".fade").each(function (i) {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 500);
+      }
+    });
+  });
 });
